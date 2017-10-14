f6a911b366d6f2d65ee59780c04f154a1dc095be
David Nguyen <hpcsc@users.noreply.github.com>
2017-10-14 23:54:23 +0800

    add additional chocolatey softwares, restore vscode settings

--- install.ps1
+++ install.ps1
@@ -1,3 +1,12 @@
+# install Chocolatey
 Set-ExecutionPolicy Bypass; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install VisualStudioCode
+# essential software
+choco install -y googlechrome firefox 7zip.install dropbox 1password vlc gom-player malwarebytes
+
+# development tools
+choco install -y VisualStudioCode linqpad sourcetree docker-for-windows nodejs.install conemu
+
+# restore vscode settings
+cmd /c mklink /H (Join-Path $Env:APPDATA "Code\User\settings.json") ".\vscode\settings.json"
+cmd /c mklink /H (Join-Path $Env:APPDATA "Code\User\keybindings.json") ".\vscode\keybindings.json"
