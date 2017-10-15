b651f3d82615ed0869b720eef3318828296cea47
David Nguyen <hungphuongjc@gmail.com>
2017-10-15 14:51:22 +0800

    install some additional tools

--- install.ps1
+++ install.ps1
@@ -2,10 +2,10 @@
 Set-ExecutionPolicy Bypass; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # essential software
-choco install -y googlechrome firefox 7zip.install dropbox 1password vlc gom-player malwarebytes
+choco install -y googlechrome firefox 7zip.install dropbox 1password vlc gom-player malwarebytes foxitreader
 
 # development tools
-choco install -y VisualStudioCode linqpad sourcetree docker-for-windows nodejs.install conemu
+choco install -y VisualStudioCode linqpad sourcetree docker-for-windows nvm conemu jetbrainstoolbox velocity boostnote
 
 # restore vscode settings
 cmd /c mklink /H (Join-Path $Env:APPDATA "Code\User\settings.json") ".\vscode\settings.json"
