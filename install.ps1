c20e0d1b0fdb804b3f732e0b6dfea3aae93664f9
hpcsc <hungphuongjc@gmail.com>
2017-10-21 22:09:21 +0800

    create vscode user folder before creating hardlink

--- install.ps1
+++ install.ps1
@@ -9,6 +9,7 @@ choco install googlechrome firefox 7zip.install dropbox 1password vlc malwarebyt
 choco install VisualStudioCode linqpad sourcetree docker-for-windows nvm conemu jetbrainstoolbox velocity boostnote jdk8
 
 # restore vscode settings
+New-Item -Path (Join-Path $Env:APPDATA "Code\User") -ItemType directory -Force
 cmd /c mklink /H (Join-Path $Env:APPDATA "Code\User\settings.json") ".\vscode\settings.json"
 cmd /c mklink /H (Join-Path $Env:APPDATA "Code\User\keybindings.json") ".\vscode\keybindings.json"
 cmd /c mklink /H (Join-Path $Env:programfiles "ConEmu\ConEmu.xml") ".\conemu\ConEmu.xml"
