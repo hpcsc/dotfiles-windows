e1e3fdd3a6614f017d1dd59bce1823fcd09f370e
hpcsc <hungphuongjc@gmail.com>
2017-10-22 15:13:10 +0800

    add customization of windows explorer

--- install.ps1
+++ install.ps1
@@ -6,4 +6,4 @@ Write-Host "Installing essential softwares..." -ForegroundColor "Yellow"
 choco install googlechrome firefox 7zip.install dropbox 1password vlc malwarebytes foxitreader
 
 Write-Host "Installing development tools..." -ForegroundColor "Yellow"
-choco install VisualStudioCode linqpad sourcetree docker-for-windows nvm conemu jetbrainstoolbox velocity boostnote jdk8
+choco install VisualStudioCode linqpad sourcetree docker-for-windows dotnetcore-sdk nvm conemu jetbrainstoolbox velocity boostnote jdk8
--- windows.ps1
+++ windows.ps1
@@ -13,3 +13,18 @@ Get-AppxPackage  -AllUsers | Where-Object Name -in $storeAppsToRemove | Remove-A
 Get-AppXProvisionedPackage -Online | Where-Object DisplayName -in $storeAppsToRemove | Remove-AppxProvisionedPackage -Online
 
 Remove-Variable storeAppsToRemove
+
+Write-Host "Updating Windows Explorer Settings..." -ForegroundColor "Yellow"
+$explorerAdvancedKey = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+Set-ItemProperty $explorerAdvancedKey LaunchTo 1            # Set "Open File Explorer to" to "This PC" instead of "Quick access"
+Set-ItemProperty $explorerAdvancedKey Start_TrackDocs 0     # Turn off "Show recently opened items in Jump Lists on Start or the taskbar"
+Set-ItemProperty $explorerAdvancedKey Start_TrackProgs 0    # Turn off "Show most used apps"
+Set-ItemProperty $explorerAdvancedKey HideFileExt 0         # Turn off "Hide extensions for known file types"
+Remove-Variable explorerAdvancedKey
+
+$explorerKey = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer'
+Set-ItemProperty $explorerKey ShowRecent 0                  # Disable "Show recent used files in Quick access"
+Set-ItemProperty $explorerKey ShowFrequent 0                # Disable "Show frequently used folders in Quick access"
+Remove-Variable explorerKey
+
+Stop-Process -processname explorer
