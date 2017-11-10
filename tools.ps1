4d75619395bf0fe39bca476486a53264b3297c90
David Nguyen <hpcsc@users.noreply.github.com>
2017-11-10 19:57:16 +0800

    fix issue with symlink vscode settings files

--- tools.ps1
+++ tools.ps1
@@ -1,7 +1,7 @@
 Write-Host "Restoring Visual Studio Code Settings..." -ForegroundColor "Yellow"
 New-Item -Path (Join-Path $Env:APPDATA "Code\User") -ItemType directory -Force
-cmd /c mklink /H (Join-Path $Env:APPDATA "Code\User\settings.json") ".\vscode\settings.json"
-cmd /c mklink /H (Join-Path $Env:APPDATA "Code\User\keybindings.json") ".\vscode\keybindings.json"
+cmd /c mklink (Join-Path $Env:APPDATA "Code\User\settings.json") (Resolve-Path ".\vscode\settings.json").Path
+cmd /c mklink (Join-Path $Env:APPDATA "Code\User\keybindings.json") (Resolve-Path ".\vscode\keybindings.json").Path
 
 Write-Host "Installing Visual Studio Code Extensions..." -ForegroundColor "Yellow"
 Get-Content ./vscode-extensions | Foreach-Object { code --install-extension $_ }
