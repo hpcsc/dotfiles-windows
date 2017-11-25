0e2e6f72dbbadbe9e99738b6f85cd07b851eb709
David Nguyen <hpcsc@users.noreply.github.com>
2017-11-25 14:05:36 +0800

    install authy

--- install.ps1
+++ install.ps1
@@ -3,7 +3,7 @@ Set-ExecutionPolicy Bypass; Invoke-Expression ((New-Object System.Net.WebClient)
 choco feature enable -n=allowGlobalConfirmation
 
 Write-Host "Installing essential softwares..." -ForegroundColor "Yellow"
-choco install googlechrome firefox 7zip.install dropbox 1password vlc malwarebytes foxitreader
+choco install googlechrome firefox 7zip.install dropbox 1password vlc malwarebytes foxitreader authy-desktop
 
 Write-Host "Installing development tools..." -ForegroundColor "Yellow"
 choco install VisualStudioCode linqpad sourcetree docker-for-windows dotnetcore-sdk nvm conemu jetbrainstoolbox velocity boostnote jdk8
