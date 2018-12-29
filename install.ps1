e5df63d3c941689d56211e296eb910ad2c895fc8
David Nguyen <hpcsc@users.noreply.github.com>
2018-12-30 01:23:18 +0800

    make chocolatey installation idempotent

--- install.ps1
+++ install.ps1
@@ -1,6 +1,14 @@
-Write-Host "Installing Chocolatey..." -ForegroundColor "Yellow"
-Set-ExecutionPolicy Bypass; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco feature enable -n=allowGlobalConfirmation
+if (Get-Command choco -errorAction SilentlyContinue)
+{
+    Write-Host "Installing Chocolatey..." -ForegroundColor "Yellow"
+    Set-ExecutionPolicy Bypass; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    choco feature enable -n=allowGlobalConfirmation
+}
+else
+{
+    Write-Host "Upgrading Chocolatey..." -ForegroundColor "Yellow"
+    choco upgrade -y chocolatey
+}
 
 Write-Host "Installing essential softwares..." -ForegroundColor "Yellow"
 choco install googlechrome firefox 7zip.install dropbox 1password vlc malwarebytes foxitreader authy-desktop greenshot
