096b2c84566bec615d25b4bf23e24b859d806db9
David Nguyen <hpcsc@users.noreply.github.com>
2019-05-26 12:22:37 +1000

    add MultiCommander startup layout

--- MultiCommander/install.ps1
+++ MultiCommander/install.ps1
@@ -4,8 +4,10 @@ Write-Host "Restoring MultiCommander ($installLocation) Settings..." -Foreground
 function BackUpAndSymlink($folderRelativePath, $fileName) {
     $folderAbsolutePath=Join-Path $installLocation $folderRelativePath
     $destinationFile=Join-Path $folderAbsolutePath $fileName
+    Write-Host $destinationFile
     if (Test-Path $destinationFile)
     {
+        Write-Host "$destinationFile exists, renaming existing file to $fileName.bk" -ForegroundColor "Yellow"
         Rename-Item $destinationFile "$fileName.bk"
     }
     cmd /c mklink $destinationFile (Resolve-Path ".\MultiCommander\$folderRelativePath\$fileName").Path
@@ -17,3 +19,4 @@ BackUpAndSymlink "Config" "UserDefinedCommands.xml"
 BackUpAndSymlink "Config" "CustomKeymappings.xml"
 BackUpAndSymlink "Config" "MultiButtons.xml"
 BackUpAndSymlink "SessionConfig\Config" "MultiCommander.ini"
+BackUpAndSymlink "" "startup.ini"
